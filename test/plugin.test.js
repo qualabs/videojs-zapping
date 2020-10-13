0add394edd865f6d37ff9870e5670b8d1f952a48
Jope algorta <josepedro@qualabs.com>
2020-10-13 15:44:54 -0300

    config set

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -46,7 +46,53 @@ QUnit.test('registers itself with video.js', function(assert) {
     'videojs-zapping plugin was registered'
   );
 
-  this.player.zapping();
+  this.player.zapping({ playlist: [
+    {
+      sources: [
+        {
+          src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+          type: 'video/mp4'
+        }
+      ],
+      poster: 'http://media.w3.org/2010/05/sintel/poster.png'
+    },
+    {
+      sources: [
+        {
+          src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+          type: 'video/mp4'
+        }
+      ],
+      poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+    },
+    {
+      sources: [
+        {
+          src: 'http://vjs.zencdn.net/v/oceans.mp4',
+          type: 'video/mp4'
+        }
+      ],
+      poster: 'http://www.videojs.com/img/poster.jpg'
+    },
+    {
+      sources: [
+        {
+          src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+          type: 'video/mp4'
+        }
+      ],
+      poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+    },
+    {
+      sources: [
+        {
+          src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+          type: 'video/mp4'
+        }
+      ],
+      poster: 'http://media.w3.org/2010/05/video/poster.png'
+    }
+  ]});
 
   // Tick the clock forward enough to trigger the player to be "ready".
   this.clock.tick(1);
